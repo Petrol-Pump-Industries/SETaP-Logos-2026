3d6ab80f545ea310ad809065df63532c3c2ed603
Goff120 <geoffreysmithuk@icloud.com>
2026-01-27 19:09:15 +0000

    out line shield

--- import turtle.py
+++ import turtle.py
@@ -1,8 +1,8 @@
 import turtle
 
 def mid_top():
-    t.home()
     t.penup()
+    t.home()
     t.left(90)
     t.forward(200)
     t.pendown()
@@ -11,16 +11,29 @@ def left_shield():
     mid_top()
     t.left(215)
     t.circle(150, 90)
+    
+    t.right(105) 
+    t.circle(-350, 90)
+    
+    
 
 def right_shield():
-    t.penup()
     mid_top()
     t.right(215)
-    t.pendown()
     t.circle(-150, 90)
+    
+    t.left(105) 
+    t.circle(350, 90)
+
+#to test where middle line is
+def test_middle():
+    mid_top()
+    t.right(180)
+    t.forward(600)
 
 
 def sheild():
+    test_middle()
     left_shield()
     right_shield()
 
